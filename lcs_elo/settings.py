888da070b2a0cee5a8fd9f7076318230cfbcea0e
Brendan <bwallace722@gmail.com>
2017-07-06 00:29:05 -0700

    revert database settings

--- lcs_elo/settings.py
+++ lcs_elo/settings.py
@@ -79,14 +79,11 @@ WSGI_APPLICATION = 'lcs_elo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bullbear',
-        'USER': 'brendan',
-        'PASSWORD': 'dbpassword',
-        'HOST': 'localhost',
-        'PORT': '', 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Internationalization
