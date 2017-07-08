922f9d838fcd2d3a605c4b150900ccb1afd01481
Brendan <bwallace722@gmail.com>
2017-07-07 19:05:35 -0700

    lcs_elo -> lolelo

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lcs_elo.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lolelo.settings")
 
     from django.core.management import execute_from_command_line
 
