051ed32d0f25fac76571c720c20aaef05b558cac
Brendan <bwallace722@gmail.com>
2017-07-13 17:08:20 -0700

    turn off debug mode

--- lolelo/settings.py
+++ lolelo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%z#bt&d$^4(#cot#cuu6rcdh25)^du+@=vnh$+r!gjoozaczt='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'lolelo.gg',
